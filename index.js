64cb5b5e9cbb044455c977981a77c4c2f88c0389
Dhea Aria Rachman <dheariarachman@gmail.com>
2023-02-02 21:08:42 +0700

    add new route

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res, next) => res.json({ message: 'Hello World' }));
+
+app.get('/product', (req, res, next) => res.json({ message: 'Product routes' }));
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log('Listening on PORT ' + PORT);
